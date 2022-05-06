9925f62f0aa2a63379797f8af1fea4854e432791
Nicolas <Vargas>
2022-05-05 20:05:17 -0500

    Inicio de salveme profe

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { IniciarSesionComponent } from './Components/iniciar-sesion/iniciar-sesion.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IniciarSesionComponent,
+    HeaderComponent 
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
